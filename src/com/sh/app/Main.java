89ed7486e1366be743515dd2077377417effd949
chayushin <cys2972@naver.com>
2023-11-13 11:45:17 +0900

    Main "Animal Go" 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -13,6 +13,7 @@ public class Main {
 		System.out.println("HELLO WORLD");
 		System.out.println("HELLO GIT");
 		
+		System.out.println("Animal Go~");
 		new Dog().bark();
 		new Cat().jump();
 		new Eagle().fly();
